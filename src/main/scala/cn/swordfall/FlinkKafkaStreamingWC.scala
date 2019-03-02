fb00f8058420dd221f90a19b393b09ec463b7a0c
swordfall <2506597416@qq.com>
2019-03-02 21:33:27 +0800

    从kafka接收数据并处理统计

--- src/main/scala/cn/swordfall/FlinkKafkaStreamingWC.scala
+++ src/main/scala/cn/swordfall/FlinkKafkaStreamingWC.scala
@@ -1,5 +1,10 @@
 package cn.swordfall
 
+import java.util.Properties
+
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
 import org.apache.log4j.{Level, Logger}
 
 /**
@@ -13,7 +18,34 @@ object FlinkKafkaStreamingWC {
     Logger.getLogger("org").setLevel(Level.OFF)
 
     //2.指定kafka数据流的相关信息
+    val kafkaCluster = "192.168.187.201:9092"
+    val topic = "test"
 
+    //3.创建流处理环境
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+    //4.创建kafka数据流
+    val props = new Properties()
+    props.put("bootstrap.servers", "192.168.187.201:9092")
+    props.put("group.id", "kv_flink")
+    props.put("enable.auto.commit", "true")
+    props.put("auto.commit.interval.ms", "1000")
+    props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+    props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+    val myConsumer = new FlinkKafkaConsumer[String](topic, new SimpleStringSchema, props)
+    val text = env.addSource(myConsumer)
+
+    //5.执行计算
+    val counts = text.flatMap(_.toLowerCase()
+      .split("\\W+"))
+      .map((_, 1))
+      .keyBy(0)
+      .sum(1)
+
+    counts.print()
+
+    //6.触发运算
+    env.execute("flink-kafka-wordcount")
   }
 
 }
