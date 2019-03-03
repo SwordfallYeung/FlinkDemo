48ae3613e4efbf00e19d206ccadd0b7a11e710f7
swordfall <2506597416@qq.com>
2019-03-04 00:49:22 +0800

    添加flink从本地集合、文件或者数据库拿取数据api

---
+++ src/main/scala/cn/swordfall/database/DataSourceFromLocalOrFile.scala
@@ -0,0 +1,110 @@
+package cn.swordfall.database
+
+import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
+
+import scala.collection.mutable
+import scala.collection.mutable.{ArrayBuffer, ListBuffer}
+
+/**
+  * @Author: Yang JianQiu
+  * @Date: 2019/3/3 21:48
+  */
+class DataSourceFromLocalOrFile {
+  def main(args: Array[String]): Unit = {
+
+  }
+
+  /**
+    * 基本本地集合的source
+    */
+  def localSource(): Unit ={
+    val senv = StreamExecutionEnvironment.getExecutionEnvironment
+    //1.用element创建DataStream.fromElements()
+    val ds1: DataStream[String] = senv.fromElements("spark", "flink")
+    ds1.print()
+
+    //2.用Tuple创建DataStream.fromElements()
+    val ds2: DataStream[(Int, String)] = senv.fromElements((1, "spark"), (2, "flink"))
+    ds2.print()
+
+    //3.用Array创建DataStream
+    val ds3: DataStream[String] = senv.fromCollection(ArrayBuffer("spark", "flink"))
+    ds3.print()
+
+    //4.用ArrayBuffer创建DataStream
+    val ds4: DataStream[String] = senv.fromCollection(ArrayBuffer("spark", "flink"))
+    ds4.print()
+
+    //5.用List创建DataStream
+    val ds5: DataStream[String] = senv.fromCollection(ListBuffer("spark", "flink"))
+    ds5.print()
+
+    //6.用Vector创建DataStream
+    val ds6: DataStream[String] = senv.fromCollection(Vector("spark", "flink"))
+    ds6.print()
+
+    //7.用Queue创建DataStream
+    val ds7: DataStream[String] = senv.fromCollection(mutable.Queue("spark", "flink"))
+    ds7.print()
+
+    //8.用Stack创建DataStream
+    val ds8: DataStream[String] = senv.fromCollection(mutable.Stack("spark", "flink"))
+    ds8.print()
+
+    //9.用Stream创建DataStream(Stream相当于lazy List，避免在中间过程中生成不必要的集合)
+    val ds9: DataStream[String] = senv.fromCollection(Stream("spark", "flink"))
+    ds9.print()
+
+    //10.用Seq创建DataStream
+    val ds10: DataStream[String] = senv.fromCollection(Seq("spark", "flink"))
+    ds10.print()
+
+    //11.用Set创建DataStream(不支持)
+    //val ds11: DataStream[String] = senv.fromCollection(Set("spark", "flink"))
+    //ds11.print()
+
+    //12.用Iterable创建DataStream(不支持)
+    //val ds12: DataStream[String] = senv.fromCollection(Iterable("spark", "flink"))
+    //ds12.print()
+
+    //13.用ArraySeq创建DataStream
+    val ds13: DataStream[String] = senv.fromCollection(mutable.ArraySeq("spark", "flink"))
+    ds13.print()
+
+    //14.用ArrayStack创建DataStream
+    val ds14: DataStream[String] = senv.fromCollection(mutable.ArrayStack("spark", "flink"))
+    ds14.print()
+
+    //15.用Map创建DataStream(不支持)
+    //val ds15: DataStream[(Int, String)] = senv.fromCollection(Map(1 -> "spark", 2 -> "flink"))
+    //ds15.print()
+
+    //16.用Range创建DataStream
+    val ds16: DataStream[Int] = senv.fromCollection(Range(1, 9))
+    ds16.print()
+
+    //17.用fromElements创建DataStream
+    val ds17: DataStream[Long] = senv.generateSequence(1, 9)
+    ds17.print()
+
+    senv.execute(this.getClass.getName)
+  }
+
+  /**
+    * 基于文件的source
+    */
+  def fileSource(): Unit ={
+    //1.创建运行环境
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+    //2.读取本地文件
+    val text1 = env.readTextFile("file:///text.txt")
+    text1.print()
+
+    //2.读取hdfs文件
+    val text2 = env.readTextFile("hdfs://192.168.187.201:9000/input/flink/text.txt")
+    text2.print()
+
+    env.execute()
+  }
+}
---
+++ src/main/scala/cn/swordfall/database/DataSourceFromMySql.scala
@@ -0,0 +1,119 @@
+package cn.swordfall.database
+
+import java.sql.{Connection, DriverManager, PreparedStatement}
+import org.apache.flink.configuration.Configuration
+import org.apache.flink.streaming.api.functions.source.{RichSourceFunction, SourceFunction}
+import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
+
+/**
+  * @Author: Yang JianQiu
+  * @Date: 2019/3/3 16:20
+  */
+class DataSourceFromMySql extends RichSourceFunction[Student]{
+  private var connection: Connection = null
+  private var ps: PreparedStatement = null
+
+  /**
+    * 一、open()方法中建立连接，这样不用每次invoke的时候都要建立连接和释放连接。
+    * @param parameters
+    */
+  override def open(parameters: Configuration): Unit = {
+    val driver = "com.mysql.jdbc.Driver"
+    val url = "jdbc:mysql://192.168.187.201:3306/test"
+    val username = "root"
+    val password = "admin"
+
+    //1.加载驱动
+    Class.forName(driver)
+    //2.创建连接
+    connection = DriverManager.getConnection(url, username, password)
+    //3.获得执行语句
+    val sql = "select stuId, stuName, stuAddr, stuSex from Student;"
+    ps = connection.prepareStatement(sql)
+  }
+
+  /**
+    *  二、DataStream调用一次run()方法用来获取数据
+    * @param sourceContext
+    */
+  override def run(sourceContext: SourceFunction.SourceContext[Student]): Unit = {
+    val resultSet = ps.executeQuery()
+    try {
+      while (resultSet.next()) {
+        val student = Student(
+          resultSet.getInt("stuId"),
+          resultSet.getString("stuName").trim,
+          resultSet.getString("stuAddr").trim,
+          resultSet.getString("stuSex").trim
+        )
+        sourceContext.collect(student)
+      }
+    } catch {
+      case e: Exception => println(e.getMessage)
+    }
+  }
+
+  override def cancel(): Unit = {
+
+  }
+
+  /**
+    *  三、 程序执行完毕就可以进行，关闭连接和释放资源的动作了
+    */
+  override def close(): Unit = {
+    //5.关闭连接和释放资源
+    if (connection != null) {
+      connection.close()
+    }
+    if (ps != null) {
+      ps.close()
+    }
+  }
+}
+
+/**
+  *        测试数据：
+  * 1.查询数据库
+  *        SHOW DATABASES;
+  *
+  * 2.创建数据库
+  *        CREATE DATABASE flinktest;
+  *
+  * 3.使用数据库
+  *        USE flinktest;
+  *
+  * 4.创建表格
+  *        CREATE TABLE Student
+  *        (
+  *        stuid INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  *        stuname VARCHAR(10) NOT NULL,
+  *        stuaddr VARCHAR(40) NOT NULL,
+  *        stusex VARCHAR(10) NOT NULL
+  *        );
+  *
+  * 5.插入数据
+  *        INSERT INTO Student(stuid,stuname,stuaddr,stusex)VALUES(1,"xiaoming","henan zhengzhou", "male")
+  *        INSERT INTO Student(stuid,stuname,stuaddr,stusex)VALUES(2,"xiaoqiang","shandong jinan", "female")
+  *        INSERT INTO Student(stuid,stuname,stuaddr,stusex)VALUES(3,"xiaohua","hebei shijiazhuang",   "male")
+  *        INSERT INTO Student(stuid,stuname,stuaddr,stusex)VALUES(4,"xiaohong","yunnan kunming",  "female")
+  *
+  * 6.查询数据
+  *        SELECT * FROM Student ;
+  */
+object DataSourceFromMySql{
+    def main(args: Array[String]): Unit = {
+      //1.创建流执行环境
+      val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+      //2.从自定义source中读取数据
+      val dataStream: DataStream[Student] = env.addSource(new DataSourceFromMySql)
+
+      //3.显示结果
+      dataStream.print()
+
+      //4.触发流执行
+      env.execute()
+    }
+}
+
+case class Student(stuId: Int, stuName: String, stuAddr: String, stuSex: String)
